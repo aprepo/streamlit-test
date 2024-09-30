5444e6fea529903175c81685787b1771170d152b
Ari-Pekka Repo <aprepo@gmail.com>
2024-09-30 14:08:14 +0300

    Add a loading spinner to fetching BG data

--- billing_groups.py
+++ billing_groups.py
@@ -4,38 +4,62 @@ from aiven.client import AivenClient
 
 client : AivenClient = st.session_state.get('client')
 
-st.title("Billing groups")
-billing_groups = client.get_billing_groups()
 
-# Fetch the invoices for all the billing groups and create a dataframe
-df = {
-    'account_name': [],
-    'billing_group_name': [],
-    'billing_type' : [],
-    'estimated_balance_usd' : [],
-    'invoices' : []
-}
+def load_billing_group_data():
+    billing_groups = client.get_billing_groups()
 
-for bg in billing_groups:
-    for col in ['account_name', 'billing_group_name', 'billing_type', 'estimated_balance_usd']:
-        df[col].append(bg.get(col))
-    bg_id = bg.get('billing_group_id')
-    total = []
-    for invoice in client.list_billing_group_invoices(bg_id):
-        timestamp = datetime.strptime(invoice.get('period_begin'), "%Y-%m-%dT%H:%M:%SZ")
-        total.append(float(invoice.get('total_inc_vat', 0)))
-    df['invoices'].append(total)
+    # Fetch the invoices for all the billing groups and create a dataframe
+    df = {
+        'account_name': [],
+        'billing_group_name': [],
+        'billing_type': [],
+        'estimated_balance_usd': [],
+        'total_spend': [],
+        'invoices': [],
+    }
 
+    for bg in billing_groups:
+        for col in ['account_name', 'billing_group_name', 'billing_type', 'estimated_balance_usd']:
+            df[col].append(bg.get(col))
+        bg_id = bg.get('billing_group_id')
+        total = []
+        total_spend = 0.0
+        for invoice in client.list_billing_group_invoices(bg_id):
+            timestamp = datetime.strptime(invoice.get('period_begin'), "%Y-%m-%dT%H:%M:%SZ")
+            spend = float(invoice.get('total_inc_vat', 0))
+            total.append(spend)
+            total_spend += spend
+        df['total_spend'].append(total_spend)
+        df['invoices'].append(total)
 
-st.dataframe(df, column_config={
+    st.dataframe(df, column_config={
         'account_name': "Account",
         'billing_group_name': "Billing group",
-        'billing_type' : "Type",
-        'estimated_balance_usd' : "Balance (USD)",
+        'billing_type': "Type",
+        'estimated_balance_usd': st.column_config.NumberColumn(
+            "Balance (in USD)",
+            help="Current estimate invoice balance in USD",
+            min_value=0,
+            step=1,
+            format="$%.2f",
+        ),
+        'total_spend': st.column_config.NumberColumn(
+            "Total spend (in USD)",
+            help="Total spend over time in USD",
+            min_value=0,
+            step=1,
+            format="$%.2f",
+        ),
         'invoices': st.column_config.BarChartColumn(
             "Historical spend"
         ),
-})
+    })
+    return df, billing_groups
 
-st.write(df)
-st.write(billing_groups)
+# Render the page. Show a spinner while the data is loading.
+st.title("Billing groups")
+with st.spinner(text="In progress..."):
+    df, billing_groups = load_billing_group_data()
+    st.write(df)
+    st.write(billing_groups)
+
