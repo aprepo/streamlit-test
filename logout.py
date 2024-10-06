8e9ab9b75ed61fd7ffd06d8ad81a2aa1bbe2f2ba
Ari-Pekka Repo <aprepo@gmail.com>
2024-10-06 16:13:50 +0300

    Remove commendted code

--- logout.py
+++ logout.py
@@ -4,7 +4,6 @@ import dialogs
 client = st.session_state.get('client')
 token_cache = st.session_state.get('token_cache')
 
-#st.write(f"Logged in as {st.session_state.get('email')}")
 user_info = client.get_user_info()
 st.write(f"Logged in as {user_info.get('user')}")
 if st.button("Log out"):
