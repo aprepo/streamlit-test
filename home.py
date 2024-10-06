84425ed21215f53d148be02895c6340b19076c6a
Ari-Pekka Repo <aprepo@gmail.com>
2024-10-06 18:57:00 +0300

    Show organization events on the home page
    The organization (account) events are fetched from the server for
    each account and merged to a full list, sorted by creation time,
    thus giving a timeline of all the changes in all the organizations
    the user has access to.
    
    The events are cached to session and there is an option to refresh
    them by a click of a button.
    
    It would make sense to fetch also the other events and merge them
    to this one, making a real full event stream that shows all events
    that have happened in all the organizations and then add a filtering
    option to the client side. These could be also cached to disk since
    these are actually immutable so these should be safe to cache forever.

--- home.py
+++ home.py
@@ -4,6 +4,17 @@ from aiven.client import AivenClient
 
 client: AivenClient = st.session_state.get('client')
 
+# Monkeypatching the client that is missing the feature I need to get the
+# organization events. This should be really contributed to the client code.
+def get_org_events(self, account_id):
+    return self.verify(
+        self.get,
+        self.build_path("account", account_id, "events"),
+        params={'limit':100},
+        result_key="events"
+    )
+AivenClient.get_org_events = get_org_events
+
 def refresh_org_cache():
     orgs = client.get_accounts()
     df = pd.DataFrame(orgs)
@@ -17,16 +28,49 @@ def refresh_org_cache():
     return df
 
 def get_org_dataframe():
-    """
-    Returns a cached org dataframe or fetches it from server if not found in
-    cache.
-    :return:
-    """
     if st.session_state.get('organizations') is None:
         with st.spinner(text="Loading..."):
             return refresh_org_cache()
     else:
         return st.session_state.get('organizations')
+
+def refresh_events(df_accounts, df):
+    all_events = pd.DataFrame([])
+    for account_id in df_accounts:
+        events = client.get_org_events(account_id)
+        if events:
+            events_df = pd.DataFrame(events).merge(
+                df[['account_id', 'account_name']],
+                on='account_id',
+                how='left'
+            )[[
+                'account_id',
+                'account_name',
+                'create_time',
+                'action_description',
+                'actor',
+                'actor_user_id'
+            ]]
+            all_events = pd.concat([all_events, events_df], ignore_index=True)
+    all_events = all_events.sort_values(
+        by='create_time',
+        ascending=False
+    )
+    st.session_state.org_events = all_events
+    return all_events
+
+def get_org_events(df_accounts, df):
+    if st.session_state.get('org_events') is None:
+        return refresh_events(df_accounts, df)
+    else:
+        return st.session_state.get('org_events')
+
+def show_org_event_stream(df_accounts, df):
+    st.title("Organization events")
+    if st.button("Refresh events"):
+        refresh_events(df_accounts, df)
+    all_events = get_org_events(df_accounts, df)
+    st.dataframe(all_events)
 
 def render_page():
     st.title("Home")
@@ -43,6 +87,9 @@ def render_page():
 
     df = get_org_dataframe()
     st.dataframe(df)
+
+    df_accounts = df['account_id'].unique()
+    show_org_event_stream(df_accounts, df)
 
 
 render_page()
